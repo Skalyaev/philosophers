abc4142852940b0dcbcaed70dbf7c7744d84ecc9
Skalya <anguinau@student.42.fr>
2024-02-03 00:07:33 +0100

    update

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Philosophers
-"I never thought philosophy would be so deadly"
+> *I never thought philosophy would be so deadly*
 
-In this project, you will learn the basics of threading a process.
-You will see how to create threads and you will discover mutexes.
+*In this project, you will learn the basics of threading a process.*
+*You will see how to create threads and you will discover mutexes.*
+
+## Usage
+```sh
+git clone git@github.com:Skalyaeve/philosophers.git
+cd philosophers
+make
+./philo time_to_die time_to_eat time_to_sleep "[number_of_times_each_philosopher_must_eat]"
+```
--- src/core/main.c
+++ src/core/main.c
@@ -14,73 +14,78 @@
 
 t_data *data(void)
 {
-        static t_data data;
+    static t_data data;
 
-        return (&data);
+    return (&data);
 }
 
 int exit_pls(void)
 {
-        if ((data())->philo)
-                free((data())->philo);
-        if ((data())->forks)
-                free((data())->forks);
-        return (0);
+    if ((data())->philo)
+        free((data())->philo);
+    if ((data())->forks)
+        free((data())->forks);
+    return (0);
 }
 
 void *exit_thread(void)
 {
-        if (pthread_mutex_lock(&(data())->death_mutex))
-                return (0);
-        (data())->someone_died = 1;
-        if (pthread_mutex_unlock(&(data())->death_mutex))
-                return (0);
-        return (NULL);
+    if (pthread_mutex_lock(&(data())->death_mutex))
+        return (0);
+    (data())->someone_died = 1;
+    if (pthread_mutex_unlock(&(data())->death_mutex))
+        return (0);
+    return (NULL);
 }
 
 int print_pls(int ms, int index, char *str)
 {
-        if (pthread_mutex_lock(&(data())->death_mutex))
-                return (0);
-        if ((data())->someone_died)
-        {
-                if (pthread_mutex_unlock(&(data())->death_mutex))
-                        return (0);
-                return (1);
-        }
-        if (ft_strchr(str, "died") != -1)
-                (data())->someone_died = 1;
+    if (pthread_mutex_lock(&(data())->death_mutex))
+        return (0);
+    if ((data())->someone_died)
+    {
         if (pthread_mutex_unlock(&(data())->death_mutex))
-                return (0);
-        if (pthread_mutex_lock(&(data())->speak_mutex))
-                return (0);
-        printf("%i ms %i %s\n", ms, index, str);
-        if (ft_strchr(str, "has taken a fork") != -1)
-                printf("%i ms %i %s\n", ms, index, str);
-        if (pthread_mutex_unlock(&(data())->speak_mutex))
-                return (0);
+            return (0);
         return (1);
+    }
+    if (ft_strchr(str, "died") != -1)
+        (data())->someone_died = 1;
+    if (pthread_mutex_unlock(&(data())->death_mutex))
+        return (0);
+    if (pthread_mutex_lock(&(data())->speak_mutex))
+        return (0);
+    printf("%i ms %i %s\n", ms, index, str);
+    if (ft_strchr(str, "has taken a fork") != -1)
+        printf("%i ms %i %s\n", ms, index, str);
+    if (pthread_mutex_unlock(&(data())->speak_mutex))
+        return (0);
+    return (1);
 }
 
 int main(int ac, char **av)
 {
-        int i;
+    int i;
 
-        if (ac > 4 && ac < 7)
-        {
-                data();
-                if (!check_input(ac, av))
-                        return (exit_pls());
-                i = -1;
-                while (++i < (data())->nbr_of_philo)
-                        pthread_create(&(data())->philo[i].thread,
-                                       NULL, philo, &(data())->philo[i]);
-                pthread_create(&(data())->time.thread, NULL, timer, &(data())->time);
-                i = -1;
-                while (++i < (data())->nbr_of_philo)
-                        pthread_join((data())->philo[i].thread, NULL);
-                pthread_join((data())->time.thread, NULL);
-                return (exit_pls());
-        }
+    if (ac > 4 && ac < 7)
+    {
+        data();
+        if (!check_input(ac, av))
+            return (exit_pls());
+        i = -1;
+        while (++i < (data())->nbr_of_philo)
+            pthread_create(&(data())->philo[i].thread,
+                           NULL, philo, &(data())->philo[i]);
+        pthread_create(&(data())->time.thread, NULL, timer, &(data())->time);
+        i = -1;
+        while (++i < (data())->nbr_of_philo)
+            pthread_join((data())->philo[i].thread, NULL);
+        pthread_join((data())->time.thread, NULL);
+        return (exit_pls());
+    }
+    else
+    {
+        puts("Usage: ./philo nbr_of_philo time_to_die time_to_eat time_to_sleep [nbr_of_times_each_philo_must_eat]");
         return (0);
+    }
+    return (0);
 }
